797004de4ea5fdf4fb8d900e05a2799f71559b8b
rogercriollo <rlcriollo@utpl.edu.ec>
2023-07-19 23:45:17 -0500

    final

--- app-django3/taller/settings.py
+++ app-django3/taller/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # agregar apps para Auth - paso 1
     #'django.contrib.sites',
-    #'allauth',
+    #'allauth', 
     #'allauth.account',
     #'allauth.socialaccount',
     #'allauth.socialaccount.providers.github',
